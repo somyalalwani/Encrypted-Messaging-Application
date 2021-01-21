ce371f2360bb7f20f6f484a0869dae97abb1a225
Mk Utkarsh <Utkarsh.mk84@gmail.com>
2021-01-21 08:26:57 +0530

    Update server.py
    Added User class for storing all information of a user and all users are stored in a list "Users"

--- server.py
+++ server.py
@@ -1,6 +1,22 @@
 import socket
 from threading import Thread
 
+class User:
+    def __init__(self, username, password, socket, address):
+        self.username = username
+        self.password = password
+        self.socket = socket
+        self.addr = address
+        self.groups = []
+    
+    def add_group(group):
+        self.groups.append(group)
+    
+    def update_socket(self,client, addr):
+        self.socket = client
+        self.addr = addr
+
+
 def rcv(client):
     while True:
         msg_rcvd = client.recv(1024).decode("utf-8")
@@ -8,8 +24,9 @@ def rcv(client):
         send(client,recipient,msg)
 
 def send(client,recipient,msg):
-    to_send = [x[1] for x in Username_and_Socket if recipient == x[0]][0]
-    sender = [x[0] for x in Username_and_Socket if client == x[1]][0]
+    to_send = [x.socket for x in Users if recipient == x.username][0]
+    sender = [x.username for x in Users if client == x.socket][0]
+    print(to_send,sender)
     msg = sender + ' : ' + msg
     to_send.send(bytes(msg,'utf-8'))
 
@@ -17,7 +34,7 @@ def client_chat(client):
     RCV_THREAD = Thread(target=rcv,args=(client,))
     RCV_THREAD.start()
 
-def client_handle(client):
+def client_handle(client,addr):
     client.send(bytes('Reply "1" for Sign Up \nReply "2" for login','utf-8'))
     user_response = client.recv(8).decode("utf-8")
     if int(user_response) == 1:
@@ -26,7 +43,7 @@ def client_handle(client):
         client.send(bytes('Enter Password','utf-8'))
         password = client.recv(8).decode("utf-8")
         Username_and_Passwords[username] = password
-        Username_and_Socket.append((username,client))
+        Users.append(User(username, password, client, addr))
         client.send(bytes('Sign Up Successful','utf-8'))
         client_chat(client)
     else:
@@ -36,24 +53,26 @@ def client_handle(client):
         password = client.recv(8).decode("utf-8")
         if username in Username_and_Passwords.keys() and Username_and_Passwords[username] == password:
             client.send(bytes('Login Successful','utf-8'))
-            Username_and_Socket.append((username,client))
+            Current_user = [x for x in Users if username == x.username][0]
+            Current_user.update_socket(client,addr)
             client_chat(client)
         elif username not in Username_and_Passwords.keys():
             client.send(bytes('Username does not exist','utf-8'))
         else:
             client.send(bytes('Password does not match','utf-8'))
-    
-    
+        
 
 def wait_for_connection():
     while True:
         client, addr = server.accept()
         client.send(bytes('Connection Established','utf-8'))
-        CLIENT_THREAD = Thread(target=client_handle,args=(client,))
+        CLIENT_THREAD = Thread(target=client_handle,args=(client,addr,))
         CLIENT_THREAD.start()
 
+#List of users
+Users = []
+
 Username_and_Passwords = {}
-Username_and_Socket = []
 PORT = 5500
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ADDR = (socket.gethostname(),PORT)
