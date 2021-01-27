a6586f19c9385a693a3875331f05e36c7552443e
Mk Utkarsh <Utkarsh.mk84@gmail.com>
2021-01-27 13:11:39 +0530

    Groups implemented

--- client.py
+++ client.py
@@ -2,79 +2,82 @@ import socket
 from threading import Thread
 import pyDH
 from Crypto.Cipher import DES3
+import sys
+import nacl.secret
+import nacl.utils
+import nacl.pwhash
+import os
+# Do not do "import nacl", it won't work
+import base64
+
+group_key={}
+
+def keyGen(password,groupname):
+    kdf = nacl.pwhash.argon2i.kdf # our key derivation function
+    salt_size = nacl.pwhash.argon2i.SALTBYTES # The salt musts have a size of 16 bytes
+    print(salt_size)
+    salt = nacl.utils.random(salt_size) # can be sth like: b'3\xba\x8f\r]\x1c\xcbOsU\x12\xb6\x9c(\xcb\x94'
+    print(salt) # To decrypt the data later, you have to save this salt somewhere.
+    password=password.encode("utf-8")
+    key = kdf(nacl.secret.SecretBox.KEY_SIZE, password, salt)
+    hexed_key = key.hex()
+    group_key[groupname] = hexed_key
+    
+
+def groupMsgGen(secret_msg,groupname):
+    key = group_key[groupname]
+    # Encrypt the data:
+    box = nacl.secret.SecretBox(bytes.fromhex(key))
+    # msgsend =''.join(format(ord(i), 'b') for i in secret_msg)
+    encrypted = box.encrypt(bytes(secret_msg.encode('utf-8')))
+    return encrypted
+
 
 def rcv():
     while True:
+        print()
+        print("-----------")
         print(server.recv(1024).decode("utf-8"))
+        print("-----------")
+        print()
+
 
 def pad(msg):
     while len(msg)%8 !=0:
         msg += ' '
     return msg 
 
-def msg_peer(peer_port,msg):
+
+def msg_peer(peer_port,msg, isFile):
     peer_ip = socket.gethostname()
     peer_info = (peer_ip,int(peer_port))
     server_peer = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server_peer.connect(peer_info)
-    DH1 = pyDH.DiffieHellman()
-    DH1_publickey = DH1.gen_public_key()
-    server_peer.send(bytes(str(DH1_publickey),'utf-8'))
-    F_publickey = server_peer.recv(1024).decode("utf-8")
+    
 
     #key for symmetric key encryption
-    DH1_secretkey = DH1.gen_shared_key(int(F_publickey))
-
-    #Encrypting Message
-    msg = pad(msg)
-    cipher = DES3.new(str(DH1_secretkey)[0:24], DES3.MODE_ECB)
-    encrypted_msg = cipher.encrypt(msg.encode("utf-8"))
-    server_peer.send(encrypted_msg)
+    
+    
+    if isFile == False:
+        msgtype = "text"
+        server_peer.send(msgtype.encode())
+        #Encrypting Message
+        DH1 = pyDH.DiffieHellman()
+        DH1_publickey = DH1.gen_public_key()
+        server_peer.send(bytes(str(DH1_publickey),'utf-8'))
+        F_publickey = server_peer.recv(1024).decode("utf-8")
+        DH1_secretkey = DH1.gen_shared_key(int(F_publickey))
+        msg = pad(msg)
+        cipher = DES3.new(str(DH1_secretkey)[0:24], DES3.MODE_ECB)
+        encrypted_msg = cipher.encrypt(msg.encode("utf-8"))
+        server_peer.send(encrypted_msg)
+    else:
+        msgtype = "file"
+        server_peer.send(msgtype.encode())
+        server_peer.send(msg)
+    # server_peer.send(msg)
     server_peer.close()
 
-
-def send(username):
-    while True:
-        msg_send = input()
-        command,msg = msg_send.split(maxsplit=1)
-        if(command == "create_group"):
-            pass
-        else:
-            server.send(bytes(command,'utf-8'))
-            details = server.recv(1024).decode("utf-8")
-            #print(details)
-            peername , portno = details.split(":")
-            message = "-----------\n "+username+':"'+msg+'" \n-----------\n'
-            msg_peer(portno,message)
-
-
-def rcv_msg(client):
-    print(client.recv(1024).decode("utf-8")) 
-
-def chat(username,portno):
-    SEND_THREAD = Thread(target=send,args=(username,))
-    #RCV_THREAD = Thread(target=rcv)
-    MSGS_THREAD = Thread(target=start_server,args=(portno,))
-    SEND_THREAD.start()
-    #RCV_THREAD.start()
-    MSGS_THREAD.start()
-
-def wait_for_connection(peer_server):
-    while True:
-        client, addr = peer_server.accept()
-        DH2 = pyDH.DiffieHellman()
-        DH2_publickey = DH2.gen_public_key()
-        R_publickey = client.recv(1024).decode("utf-8")
-        client.send(bytes(str(DH2_publickey),'utf-8'))
-
-        #key for symmetric key decryption
-        shared_key2 = DH2.gen_shared_key(int(R_publickey))
-        encrypted_msg = client.recv(1024)
-
-        #decrypting message
-        cipher = DES3.new(str(shared_key2)[0:24], DES3.MODE_ECB)
-        msg = cipher.decrypt(encrypted_msg)
-        print(str(msg.decode("utf-8")))
 
 def start_server(portno):
     PORT1 = int(portno)
@@ -89,52 +92,206 @@ def start_server(portno):
         #client1.send(bytes('Connection Established','utf-8'))
         #CLIENT_THREAD = Thread(target=rcv_msg,args=(peer_server,))
         #CLIENT_THREAD.start()
-    
 
-    
+def send(username):
+    while True:
+        msg_send = input()
+        command,msg = msg_send.split(maxsplit=1)
+        command = command.lower()
+        if (command == 'send'):
+            isFile = False
+            command, msg = msg.split(maxsplit=1)
+            server.send(bytes(command,'utf-8'))
+            details = server.recv(1024).decode("utf-8")
+            peername , portno = details.split(":")
 
-PORT = input('Enter port: ')
+            if 'file' in msg: #ruchi file a.txt
+                isFile = True
+                filename = msg.split(' ')[1]
+                header = username+ ' file ' + filename+'\n\n' #file a.txt\n\n
+                if isFile == True:
+                    
+                    try:
+                        fin = open(filename,"rb")
+                        content = fin.read()
+                        content = header.encode()+content
+                        fin.close()
+                        msg_peer(portno,content,isFile)
+
+                    except FileNotFoundError:
+                        content = 'unsuccessfull recieve :(\n'
+                        msg_peer(portno,content,isFile)
+            else:
+                message = "\n-----------\n" + username + ':"' + msg + '"\n-----------\n'
+                msg_peer(portno,message,isFile)
+
+        elif(command=="create"):
+            server.send(bytes(str(command) + " " + str(msg),'utf-8'))
+            details = server.recv(1024).decode("utf-8") #key recvd
+            if(details=="Group creation Successful! Generate a key :"):
+                password = username+msg
+                keyGen(password,msg)
+
+
+            else:
+                print("error!")
+        elif(command=="list"):
+            server.send(bytes(str(command)+" "+ str(msg),'utf-8'))
+            details = server.recv(1024).decode("utf-8")
+            print()
+            print("-----------")
+            print(" Group: No. of members")
+            print(details)
+            print("-----------")
+            print()
+
+
+        elif(command=="join"):
+            server.send(bytes(str(command) + " "+ str(msg),'utf-8'))
+            admin_port = server.recv(1024).decode()
+            print("************:")
+            print(admin_port)
+            print("************")
+            if admin_port != "Group creation Successful! Generate a key :":
+                peer_ip = socket.gethostname()
+                peer_info = (peer_ip,int(admin_port))
+                server_peer = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                server_peer.connect(peer_info)
+                txt="keyg"
+                server_peer.send(txt.encode())
+                server_peer.send(msg.encode()) #sending grp name
+                details=server_peer.recv(1024).decode() #key
+                print()
+                print("-----------")
+                print("Group joined successfully!!")
+                # print("Key recvd : "+ details)
+                print("-----------")
+                print()
+                group_key[msg] = details
+                server_peer.close()
+                
+                
+            else:
+                print("key generation code ayega yaha bs")
+
+            
+
+        else:
+            gname,secret_msg = msg.split(maxsplit=1)
+            encrypted = groupMsgGen(secret_msg,gname)
+            server.send(bytes(str(command) + " "+ str(gname),'utf-8'))
+            server.send(encrypted)
+            details = server.recv(1024).decode()
+            #print(details)
+            
+
+def rcv_msg(client):
+    data = client.recv(1024) #when msg is multimedia
+    header,content = data.split(b'\n\n',1)
+    header = header.decode() # username file a.txt
+    # print(header)
+    list1 = header.split(' ')
+    if 'file' in header:
+        uname = list1[1]
+        filename = header.split(' ')[2]
+        ext = filename.split('.')[1]
+        newfilename = filename.split('.')[0] + "(new)."+ext #a(new).txt
+
+        with open(newfilename, "wb") as F:
+            while content:
+                F.write(content)
+                content = client.recv(1024)
+            F.close()
+            print("\n-----------\n" + uname + ':"' + newfilename+' recieved!' + '"\n-----------\n')
+            # print("Your friend has sent " + old_filename + " which has been downloaded on your system as " + filename)
+    
+    else:
+        print(content.decode("utf-8")) 
+
+def chat(username,portno):
+    SEND_THREAD = Thread(target=send,args=(username,))
+    MSGS_THREAD = Thread(target=start_server,args=(portno,))
+    SEND_THREAD.start()
+    MSGS_THREAD.start()
+
+def wait_for_connection(peer_server):
+    while True:
+         client, addr = peer_server.accept()
+         filetype = client.recv(4)
+         filetype = filetype.decode()
+         #print(filetype)
+         if filetype == "text":
+             DH2 = pyDH.DiffieHellman()
+             DH2_publickey = DH2.gen_public_key()
+             R_publickey = client.recv(1024).decode("utf-8")
+             client.send(bytes(str(DH2_publickey),'utf-8'))
+             #key for symmetric key decryption
+             shared_key2 = DH2.gen_shared_key(int(R_publickey))
+             encrypted_msg = client.recv(1024)
+             #decrypting message
+             cipher = DES3.new(str(shared_key2)[0:24], DES3.MODE_ECB)
+             msg = cipher.decrypt(encrypted_msg)
+             print(str(msg.decode("utf-8")))
+         elif filetype == "file":
+            rcv_msg(client)
+         elif filetype=="grup":
+            data = (client.recv(1024).decode())
+            msg = client.recv(1048576)
+            print()
+            print("-----------")
+            gname = data.split(" [")[0]
+            box = nacl.secret.SecretBox(bytes.fromhex(group_key[gname]))
+            #encrypted = base64.b64decode(msg)
+            secret_msg = box.decrypt(msg)
+            temp = data.split(":")[0] + ": " + secret_msg.decode()
+            print(temp)
+            print("-----------")
+            print()
+
+         elif filetype=="keyg":
+            grpname = client.recv(1024).decode()
+            #print("Enter key to join group" + grpname)
+            data=group_key[grpname]
+            # print("KEY SENT:",data)
+            client.send(str(data).encode())
+
+         else:
+             print(filetype)
+             print("message type not recognised!!")
+
+PORT = input('Enter server\'s port: ')
 ADDR = (socket.gethostname(),int(PORT))
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.connect(ADDR)
-print(server.recv(1024).decode("utf-8"))
-
+print(server.recv(1024).decode("utf-8")) #will print connection established if established
 
 #LOGIN or SIGNUP
-print(server.recv(1024).decode("utf-8"))
-sending = input()
-server.send(bytes(sending,"utf-8"))
+user_Data = []
+user_response = input('Reply "1" for Sign Up \nReply "2" for login\n')
+if user_response == '1':
+    username = input('enter username\n')
+    password = input('enter password\n')
+    portno = input('enter your own port\n')
+    user_Data = [ '1',username, password, portno]
+    server.send(bytes(str(user_Data),'utf-8'))
+    # server.send(str(userdata).encode())
 
-#SIGNUP
-def login():
-    if int(sending) == 1:
-        print(server.recv(1024).decode("utf-8"))
-        username = input()
-        server.send(bytes(username,'utf-8'))
-        print(server.recv(1024).decode("utf-8"))
-        password = input()
-        server.send(bytes(password,'utf-8'))
-        print(server.recv(1024).decode("utf-8"))
-        port_no = input()
-        server.send(bytes(port_no,'utf-8'))
-        server_response = server.recv(1024).decode("utf-8")
-        print(server_response)
-    elif int(sending) == 2:
-        print(server.recv(1024).decode("utf-8"))
-        username = input()
-        server.send(bytes(username,'utf-8'))
-        print(server.recv(1024).decode("utf-8"))
-        password = input()
-        server.send(bytes(password,'utf-8'))
-        print(server.recv(1024).decode("utf-8"))
-        port_no = input()
-        server.send(bytes(port_no,'utf-8'))
-        server_response = server.recv(1024).decode("utf-8")
-        print(server_response)
-    return server_response,username,port_no
+elif user_response == '2':
+    username = input('enter username\n')
+    password = input('enter password\n')
+    portno = input('enter your own port\n')
+    user_Data = ['2',username, password, portno]
+    server.send(bytes(str(user_Data),'utf-8'))
+else:
+    print("invalid response!")
+    sys.exit()
 
-server_response,username,portno = login()
+server_response = server.recv(1024).decode("utf-8") #server will tell if login/signup is successful or not
+print()
+print(server_response)
+
 if("Successful" in server_response):
+    print("\n***chatroom started! say hii to your friend!***\n")
     chat(username,portno)
     
--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 import socket
 from threading import Thread
+import pyDH
+from Crypto.Cipher import DES3
 
+def pad(msg):
+    while len(msg) != 1024:
+        msg += ' '
+    return msg 
+
+Group_lists=[] #key as grp name and value and name of all group members
+Username_and_Port = {}
 class User:
     def __init__(self, username, password, port, socket, address):
         self.username = username
@@ -18,13 +27,85 @@ class User:
         self.addr = addr
         self.port = port
 
+class Groups:
+    def __init__(self,group_name,username):
+        self.group_name=group_name
+        self.members=[]
+        self.members.append(username)
+        self.admin = Username_and_Port[username]
+
+    def update_group(self,username):
+        self.members.append(username)
+
 
 def rcv(client,username):
     while True:
         msg_rcvd = client.recv(1024).decode("utf-8")
         #(recipient, msg1) = msg_rcvd.split(maxsplit=1)
         #print(msg_rcvd)
-        send(client,username,msg_rcvd)
+        if not(msg_rcvd.startswith("create") or msg_rcvd.startswith("group") or msg_rcvd.startswith("join") or msg_rcvd.startswith("list")):
+            send(client,username,msg_rcvd)
+        else:
+            (command, msg) = msg_rcvd.split(maxsplit=1) 
+            
+            if (command=="create"):
+                Group_lists.append(Groups(msg,username))
+                """
+                for obj in Group_lists:
+                    if obj.group_name == msg :
+                        client.send(bytes(obj.key,'utf-8'))
+                """
+                for obj in Users:
+                    if obj.username==username:
+                        obj.groups.append(msg)
+                print("***********Group created****************")
+                client.send(bytes("Group creation Successful! Generate a key :",'utf-8'))
+
+
+            elif(command=="group"): #msg=groupname msg # command=group
+                i=0
+                groupname=msg
+                msg1 = client.recv(1048576)
+                for i in range(len(Group_lists)):
+                    if(Group_lists[i].group_name==groupname):
+                        if username in Group_lists[i].members:
+                            send1("group",username,groupname,msg1) 
+                            client.send("Message sent without encryption".encode())
+                            print("***********Message sent****************")
+                        else:
+                            client.send("You are not part of the group".encode())
+
+            elif(command=="join"): #msg=group ka naam
+                i=0
+                for i in range(len(Group_lists)):
+                    if(Group_lists[i].group_name==msg):
+                        Group_lists[i].update_group(username)
+                        admin_port=str(Group_lists[i].admin)
+                        client.send(admin_port.encode())
+                        print("***************Group joined********************")
+                if(i==len(Group_lists)):
+                    Group_lists[i].append(Groups(groupname,username))
+                    """
+                    for obj in Group_lists:
+                        if obj.group_name == groupname :
+                            y=str(obj.key)
+                            client.send(y.encode())
+                    """
+                    for obj in Users:
+                        if obj.username==username:
+                            obj.groups.append(msg)
+                    print("***********Group created****************")
+                    client.send("Group creation Successful! Generate a key :".encode())
+
+            elif(command=="list"):
+                data = {}
+
+                for x in Group_lists:
+                    data[x.group_name] = len(x.members)
+                #length=int(len(data))
+                client.send(str(data).encode())
+                
+
 
 def send(client,username,peername):
     to_send = [x.socket for x in Users if username == x.username][0]
@@ -35,32 +116,66 @@ def send(client,username,peername):
     m1 = peername + ":" + reciever_port
     to_send.send(bytes(m1,'utf-8'))
 
+
+def msg_peer1(peer_port,msg):
+    
+    """
+    DH1 = pyDH.DiffieHellman()
+    DH1_publickey = DH1.gen_public_key()
+    server_peer.send(bytes(str(DH1_publickey),'utf-8'))
+    F_publickey = server_peer.recv(1024).decode("utf-8")
+
+    #key for symmetric key encryption
+    DH1_secretkey = DH1.gen_shared_key(int(F_publickey))
+
+    #Encrypting Message
+    msg = pad(msg)
+    cipher = DES3.new(str(DH1_secretkey)[0:24], DES3.MODE_ECB)
+    encrypted_msg = cipher.encrypt(msg.encode("utf-8"))
+    server_peer.send(encrypted_msg)
+    """
+    # server_peer.send(msg)
+    # server_peer.close()
+
+
+def send1(txt,client,groupname,msg):
+    
+    if(txt=="group"):
+        for obj in Group_lists:
+            if obj.group_name==groupname:
+                txt="grup"
+                msg2 = str(groupname) + " [" + str(client) +"] : "
+                msg2 = pad(msg2) 
+                for mem in obj.members:    
+                    if mem != client:
+                        peer_port = Username_and_Port[mem]
+                        peer_ip = socket.gethostname()
+                        peer_info = (peer_ip,int(peer_port))
+                        server_peer = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                        server_peer.connect(peer_info)
+                        server_peer.send(txt.encode())
+                        server_peer.send(msg2.encode())
+                        server_peer.send(msg)
+                        server_peer.close()
+
+
 def client_chat(client,username):
     RCV_THREAD = Thread(target=rcv,args=(client,username,))
     RCV_THREAD.start()
 
 def client_handle(client,addr):
-    client.send(bytes('Reply "1" for Sign Up \nReply "2" for login','utf-8'))
-    user_response = client.recv(8).decode("utf-8")
-    if int(user_response) == 1:
-        client.send(bytes('Enter Username','utf-8'))
-        username = client.recv(8).decode("utf-8")
-        client.send(bytes('Enter Password','utf-8'))
-        password = client.recv(8).decode("utf-8")
-        client.send(bytes('Enter Port','utf-8'))
-        port= client.recv(8).decode("utf-8")
+    user_response = client.recv(4028).decode("utf-8")
+    user_response = user_response.replace("', '", " ").strip('[]').strip("''").split(' ')
+    username = user_response[1]
+    password = user_response[2]
+    port = user_response[3]
+    if int(user_response[0]) == 1:
         Username_and_Passwords[username] = password
         Username_and_Port[username] = port
         Users.append(User(username, password, port, client, addr))
         client.send(bytes('Sign Up Successful','utf-8'))
         client_chat(client,username)
     else:
-        client.send(bytes('Enter Username','utf-8'))
-        username = client.recv(8).decode("utf-8")
-        client.send(bytes('Enter Password','utf-8'))
-        password = client.recv(8).decode("utf-8")
-        client.send(bytes('Enter Port','utf-8'))
-        port= client.recv(8).decode("utf-8")
         Username_and_Port[username] = port
         if username in Username_and_Passwords.keys() and Username_and_Passwords[username] == password:
             client.send(bytes('Login Successful','utf-8'))
@@ -71,7 +186,6 @@ def client_handle(client,addr):
             client.send(bytes('Username does not exist','utf-8'))
         else:
             client.send(bytes('Password does not match','utf-8'))
-        
 
 def wait_for_connection():
     while True:
@@ -83,11 +197,15 @@ def wait_for_connection():
 #List of users
 Users = []
 
+
+
 Username_and_Passwords = {}
-Username_and_Port = {}
-PORT = 5500
+
+PORT = 5507
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 ADDR = (socket.gethostname(),PORT)
+
 server.bind(ADDR)
 
 if __name__ == "__main__":
